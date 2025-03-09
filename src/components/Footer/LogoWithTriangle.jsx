f6220396bf3e207f46d57a9f88ec8408d33b15a1
kwamedanso <dansokwame96@gmail.com>
2025-03-09 12:13:08 +0000

    Replace footer logo

--- src/components/Footer/LogoWithTriangle.jsx
+++ src/components/Footer/LogoWithTriangle.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/LogoWithTriangle.css';
+import { TbNavigationFilled } from "react-icons/tb";
+
 
 const LogoWithTriangle = () => {
     return (
         <a href="/" className="logo-link">
             <div className="logo-with-triangle">
                 <span className="logo-text">NaviTek</span>
-                <span className="logo-triangle"></span>
+                <TbNavigationFilled className='logo-svg-icon' />
             </div>
         </a>
     );
