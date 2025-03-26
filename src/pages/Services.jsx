38b2ebef304813430e512106f5354eadc7843b5a
kwamedanso <dansokwame96@gmail.com>
2025-03-26 16:52:16 +0000

    remove hero section from bottom of the page"

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -19,7 +19,6 @@ export default function Services() {
             <ServicesOffered />
             <WhyChooseUs />
             <FaqSection />
-            <HeroSection />
         </div>
     )
 }
